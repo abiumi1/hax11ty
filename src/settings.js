60c1123c817e01d1078b3a0cfa4973a73ead0f8e
Pesanproperty <newpropertysyariah@gmail.com>
2024-06-20 10:57:33 +0700

    setting

--- src/settings.js
+++ src/settings.js
@@ -5,7 +5,7 @@ module.exports = () => {
     // will most likley want to change designated
     // github organization / username
     // github.com/WHATEVER
-    var gitOrg = "elmsln";
+    var gitOrg = "abiumi1";
     // github.com/elmsln/WHATEVER - hax11ty here implies building against the repo itself to produce it's own docs :)
     var gitProject = "hax11ty";
     // if doing github builds, automatically set these to whatever the repo issuing the call is
